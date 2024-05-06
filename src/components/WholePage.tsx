23722a8600b4d4cec7e8b831746492be93cc7ed1
Mahmoudamin11 <mahmoudamin123321@gmail.com>
2024-05-06 06:54:38 +0300

    menu scroll disable

--- src/components/WholePage.tsx
+++ src/components/WholePage.tsx
@@ -8,18 +8,18 @@ import Footer from './Footer';
 import Menu from './Menu';
 import { AnimatePresence } from 'framer-motion';
 export const WholePage = () => {
-  const { getThemeChangerState, getCurrTheme , menuOpen} = useCont();
+  const { getThemeChangerState, getCurrTheme, menuOpen } = useCont();
   const loc = useLocation();
-  
+
   return (
-    <div className={`relative ${menuOpen ? "max-h-[100vh] overflow-hidden" : ""}  ${getCurrTheme() == 8 ? "bg-rainbow" : "bg-main-background"} ${getCurrTheme() == 9 ? "cursor-lobster" : ""} z-20 w-full min-h-[100vh] overflow-x-hidden ${getThemeChangerState() == 1 ? "mt-44" : "mt-0"} trans `}>
+    <div className={`relative ${menuOpen ? "max-h-[80vh] overflow-hidden" : ""}  ${getCurrTheme() == 8 ? "bg-rainbow" : "bg-main-background"} ${getCurrTheme() == 9 ? "cursor-lobster" : ""} z-20 w-full min-h-[100vh] overflow-x-hidden ${getThemeChangerState() == 1 ? "mt-44" : "mt-0"} trans `}>
       <Navbar />
-      
+
       <AnimatePresence>
         <Routes location={loc} key={loc.pathname}>
-          <Route path="/" element = {<Home />} />
-          <Route path="/portfolio" element = {<Portfolio />} />
-          <Route path="/about" element = {<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/about" element={<About />} />
         </Routes>
         {menuOpen && <Menu />}
       </AnimatePresence>
