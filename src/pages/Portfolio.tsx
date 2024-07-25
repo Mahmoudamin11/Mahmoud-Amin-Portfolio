009a9e56f13d85dfaa92a1cd9fb31316b84f8f42
Mahmoudamin11 <mahmoudamin123321@gmail.com>
2024-07-25 09:03:13 +0300

    fixing redux logo

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -129,7 +129,7 @@ const Portfolio = memo(() => {
         <svg viewBox="0 0 128 128" className="w-10 trans max-sm:hidden animate-bounce">
           <path fill="#0acf83" d="M45.5 129c11.9 0 21.5-9.6 21.5-21.5V86H45.5C33.6 86 24 95.6 24 107.5S33.6 129 45.5 129zm0 0"></path><path fill="#a259ff" d="M24 64.5C24 52.6 33.6 43 45.5 43H67v43H45.5C33.6 86 24 76.4 24 64.5zm0 0"></path><path fill="#f24e1e" d="M24 21.5C24 9.6 33.6 0 45.5 0H67v43H45.5C33.6 43 24 33.4 24 21.5zm0 0"></path><path fill="#ff7262" d="M67 0h21.5C100.4 0 110 9.6 110 21.5S100.4 43 88.5 43H67zm0 0"></path><path fill="#1abcfe" d="M110 64.5c0 11.9-9.6 21.5-21.5 21.5S67 76.4 67 64.5 76.6 43 88.5 43 110 52.6 110 64.5zm0 0"></path>
         </svg>
-        <img src="/src/assets/portfolio/redux-logo-svgrepo-com.svg" alt="" className="w-[40px] h-[40px] animate-bounce" />
+        <img src="../assets/portfolio/redux-logo-svgrepo-com.svg" alt="" className="w-[40px] h-[40px] animate-bounce" />
         {/* add github as an icon  */}
         <FontAwesomeIcon icon={faGithub} className="text-[40px] max-sm:hidden text-text-color trans animate-bounce" />
         
@@ -159,7 +159,7 @@ const Portfolio = memo(() => {
             <svg viewBox="0 0 128 128" className="w-10 trans animate-bounce">
               <path fill="#fff" d="M22.67 47h99.67v73.67H22.67z"></path><path data-name="original" fill="#007acc" d="M1.5 63.91v62.5h125v-125H1.5zm100.73-5a15.56 15.56 0 017.82 4.5 20.58 20.58 0 013 4c0 .16-5.4 3.81-8.69 5.85-.12.08-.6-.44-1.13-1.23a7.09 7.09 0 00-5.87-3.53c-3.79-.26-6.23 1.73-6.21 5a4.58 4.58 0 00.54 2.34c.83 1.73 2.38 2.76 7.24 4.86 8.95 3.85 12.78 6.39 15.16 10 2.66 4 3.25 10.46 1.45 15.24-2 5.2-6.9 8.73-13.83 9.9a38.32 38.32 0 01-9.52-.1 23 23 0 01-12.72-6.63c-1.15-1.27-3.39-4.58-3.25-4.82a9.34 9.34 0 011.15-.73L82 101l3.59-2.08.75 1.11a16.78 16.78 0 004.74 4.54c4 2.1 9.46 1.81 12.16-.62a5.43 5.43 0 00.69-6.92c-1-1.39-3-2.56-8.59-5-6.45-2.78-9.23-4.5-11.77-7.24a16.48 16.48 0 01-3.43-6.25 25 25 0 01-.22-8c1.33-6.23 6-10.58 12.82-11.87a31.66 31.66 0 019.49.26zm-29.34 5.24v5.12H56.66v46.23H45.15V69.26H28.88v-5a49.19 49.19 0 01.12-5.17C29.08 59 39 59 51 59h21.83z"></path>
             </svg>
-              <img src="/src/assets/portfolio/redux-logo-svgrepo-com.svg" alt="" className="w-[40px] h-[40px] animate-bounce" />
+              <img src="../assets/portfolio/redux-logo-svgrepo-com.svg" alt="" className="w-[40px] h-[40px] animate-bounce" />
             <svg viewBox="0 0 128 128" className="w-10 trans animate-bounce">
               <path fill="#0acf83" d="M45.5 129c11.9 0 21.5-9.6 21.5-21.5V86H45.5C33.6 86 24 95.6 24 107.5S33.6 129 45.5 129zm0 0"></path><path fill="#a259ff" d="M24 64.5C24 52.6 33.6 43 45.5 43H67v43H45.5C33.6 86 24 76.4 24 64.5zm0 0"></path><path fill="#f24e1e" d="M24 21.5C24 9.6 33.6 0 45.5 0H67v43H45.5C33.6 43 24 33.4 24 21.5zm0 0"></path><path fill="#ff7262" d="M67 0h21.5C100.4 0 110 9.6 110 21.5S100.4 43 88.5 43H67zm0 0"></path><path fill="#1abcfe" d="M110 64.5c0 11.9-9.6 21.5-21.5 21.5S67 76.4 67 64.5 76.6 43 88.5 43 110 52.6 110 64.5zm0 0"></path>
             </svg>
